433d03618e5615340c2d25955d5739da64ba9b7c
Artem Babak <artbbk27@gmail.com>
2023-10-17 15:49:33 +0300

    local train works

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -14,17 +14,23 @@ def collate_fn(dataset_items: List[dict]):
         'spectrogram': [],
         'text_encoded': [],
         'text': [],
-        'text_encoded_length': []
+        'text_encoded_length': [],
+        'spectrogram_length': [],
+        'audio_path': []
     }
 
     for item in dataset_items: 
         result_batch['spectrogram'].append(item['spectrogram'].squeeze(0).T)
         result_batch['text_encoded'].append(item['text_encoded'].view(-1,))
         result_batch['text'].append(item['text'])
+        result_batch['audio_path'].append(item['audio_path'])
         result_batch['text_encoded_length'].append(item['text_encoded'].shape[0])
+        result_batch['spectrogram_length'].append(result_batch['spectrogram'][-1].shape[0])
+
     
     result_batch['text_encoded'] = pad_sequence(result_batch['text_encoded'], True, 0)
     result_batch['spectrogram'] = pad_sequence(result_batch['spectrogram'], True, 0).transpose(1, 2)
     result_batch['text_encoded_length'] = torch.tensor(result_batch['text_encoded_length'])
+    result_batch['spectrogram_length'] = torch.tensor(result_batch['spectrogram_length'])
     
     return result_batch
