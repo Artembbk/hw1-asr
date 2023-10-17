ea159d6dc2377194bf61f2889e1c31fefd7c2352
Artem Babak <artbbk27@gmail.com>
2023-10-18 01:03:00 +0300

    deep speech v.01

--- hw_asr/model/deepspeech.py
+++ hw_asr/model/deepspeech.py
@@ -15,29 +15,29 @@ class DeepSpeech2pacModel(BaseModel):
         self.stride1 = (2, 2)
         self.kernel_size2 = (21, 11)
         self.stride2 = (2, 1)
-        self.kernel_size3 = (21, 11)
+        # self.kernel_size3 = (21, 11)
         self.stride3 = (2, 1)
         self.channels1 = 32
         self.channels2 = 32
-        self.channels3 = 96
+        # self.channels3 = 96
         self.fc_hidden = fc_hidden
         self.n_class = n_class
 
         self.out_dim1 = ((dim - self.kernel_size1[0]) // self.stride1[0]) + 1
         self.out_dim2 = ((self.out_dim1 - self.kernel_size2[0]) // self.stride2[0]) + 1
-        self.out_dim3 = ((self.out_dim2 - self.kernel_size3[0]) // self.stride3[0]) + 1
+        # self.out_dim3 = ((self.out_dim2 - self.kernel_size3[0]) // self.stride3[0]) + 1
 
         self.conv1 = nn.Conv2d(1, self.channels1, self.kernel_size1, stride=self.stride1)
         self.conv2 = nn.Conv2d(self.channels1, self.channels2, self.kernel_size2, stride=self.stride2)
-        self.conv3 = nn.Conv2d(self.channels2, self.channels3, self.kernel_size3, stride=self.stride3)
+        # self.conv3 = nn.Conv2d(self.channels2, self.channels3, self.kernel_size3, stride=self.stride3)
 
         self.batchnorm1 = nn.BatchNorm2d(self.channels1)
         self.batchnorm2 = nn.BatchNorm2d(self.channels2)
-        self.batchnorm3 = nn.BatchNorm2d(self.channels3)
+        # self.batchnorm3 = nn.BatchNorm2d(self.channels3)
 
         self.relu = nn.ReLU()
 
-        self.rnn = nn.GRU(self.out_dim3 * self.channels3, fc_hidden, num_layers=1, batch_first=True)
+        self.rnn = nn.GRU(self.out_dim2 * self.channels2, fc_hidden, num_layers=1, batch_first=True)
 
         self.fc = nn.Linear(in_features=fc_hidden, out_features=n_class)
 
@@ -52,16 +52,16 @@ class DeepSpeech2pacModel(BaseModel):
         assert output.size()[2] == self.out_dim1
         output = self.relu(self.conv2(output))
         assert output.size()[2] == self.out_dim2
-        output = self.relu(self.conv3(output))
-        assert output.size()[2] == self.out_dim3
+        # output = self.relu(self.conv3(output))
+        # assert output.size()[2] == self.out_dim3
 
         batch_size, channels, dim, seq_l = output.size()
-        assert channels == self.channels3
+        assert channels == self.channels2
         assert batch_size == len(batch['audio_path'])
         output = output.permute(0, 3, 1, 2)
         output = output.view(batch_size, seq_l, channels * dim)
 
-        output = self.rnn(output)
+        output, _ = self.rnn(output)
         batch_size, seq_l, dim = output.size()
         assert batch_size == len(batch['audio_path'])
         assert dim == self.fc_hidden
@@ -69,9 +69,11 @@ class DeepSpeech2pacModel(BaseModel):
         batch_size, seq_l, dim = output.size()
         assert batch_size == len(batch['audio_path'])
         assert dim == self.n_class
-
+        self.time_out = seq_l
         return output
 
 
     def transform_input_lengths(self, input_lengths):
-        return input_lengths  # we don't reduce time dimension here
+        out_time1 = ((input_lengths - self.kernel_size1[1]) // self.stride1[1]) + 1
+        out_time2 = ((out_time1 - self.kernel_size2[1]) // self.stride2[1]) + 1
+        return out_time2  # we don't reduce time dimension here
