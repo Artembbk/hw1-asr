c6eec9c9bf6528ec781f8f916a7b2a367719c9d8
Artem Babak <artbbk27@gmail.com>
2023-10-22 15:13:01 +0300

    packed

--- hw_asr/model/deepspeech.py
+++ hw_asr/model/deepspeech.py
@@ -1,83 +1,63 @@
 from torch import nn
 from torch.nn import Sequential
 from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
 import torch
 
 from hw_asr.base import BaseModel
 
-class CustomGRU(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(CustomGRU, self).__init__()
+
+class BatchRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, rnn_type=nn.LSTM, bidirectional=False, batch_norm=True, batch_first=True):
+        super(BatchRNN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        
-        # Веса для обновления состояния (Update Gate)
-        self.W_z = nn.Linear(hidden_size, hidden_size)
-        self.U_z = nn.Linear(input_size, hidden_size)
-        
-        # Веса для создания нового состояния (New State)
-        self.W_r = nn.Linear(hidden_size, hidden_size)
-        self.U_r = nn.Linear(input_size, hidden_size)
-        self.W_h = nn.Linear(hidden_size, hidden_size)
-        self.U_h = nn.Linear(input_size, hidden_size)
-        self.bn = nn.BatchNorm1d(hidden_size)
-
-    def forward(self, x, h):
-        # Расчет вектора обновления (Update Gate)
-        z = torch.sigmoid(self.W_z(h) + self.bn(self.U_z(x)))
-        
-        # Расчет вектора сброса (Reset Gate)
-        r = torch.sigmoid(self.W_r(h) + self.bn(self.U_r(x)))
-        
-        # Расчет нового состояния (New State)
-        new_state = torch.tanh(self.W_h(r * h) + self.bn(self.U_h(x)))
-        
-        # Обновление текущего состояния (Hidden State Update)
-        h_new = (1 - z) * h + z * new_state
-        
-        return h_new
-    
-import torch
-import torch.nn as nn
-
-class CustomGRULayer(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=1, bidirectional=False, batch_first=True):
-        super(CustomGRULayer, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
         self.bidirectional = bidirectional
-        self.directions = 2 if bidirectional else 1  # 2 for bidirectional, 1 for unidirectional
-        self.gru_cells = nn.ModuleList([CustomGRU(input_size, hidden_size) for _ in range(self.directions * num_layers)])
+        self.batch_norm = nn.BatchNorm1d(input_size) if batch_norm else None
+        self.rnn = rnn_type(input_size=input_size, hidden_size=hidden_size,
+                            bidirectional=bidirectional, bias=True, batch_first=batch_first)
+        self.num_directions = 2 if bidirectional else 1
 
-    def forward(self, x, h=None):
-        if h is None:
-            h = torch.zeros(self.directions * self.num_layers, batch_size, self.hidden_size)
-        # If bidirectional, we need to account for the forward and backward directions.
+    def forward(self, x, output_lengths, h=None):
+        if self.batch_norm is not None:
+            x = x.transpose(1, 2)
+            x = self.batch_norm(x)
+            x = x.transpose(1, 2)
+        x = pack_padded_sequence(x, output_lengths, batch_first=True, enforce_sorted=False)
+        x, h = self.rnn(x, h)
+        x, output_lengths = pad_packed_sequence(x, batch_first=True)
         if self.bidirectional:
-            self.directions = 2
+            x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)
+        return x, h
 
-        # Get the number of time steps and batch size
-        seq_len, batch_size, _ = x.size()
+class BatchRNNLayer(nn.Module):
+    def __init__(self, input_size, hidden_size, rnn_type=nn.RNN, bidirectional=False, num_layers = 1, batch_first=True):
+        super(BatchRNNLayer, self).__init__()
+        self.rnn = nn.Sequential(
+            BatchRNN(
+                input_size=input_size,
+                hidden_size=hidden_size,
+                rnn_type=rnn_type,
+                bidirectional=bidirectional,
+                batch_norm=False,
+                batch_first=batch_first
+            ),
+            *(
+                BatchRNN(
+                    input_size=hidden_size,
+                    hidden_size=hidden_size,
+                    rnn_type=rnn_type,
+                    bidirectional=bidirectional,
+                    batch_first=batch_first
+                ) for _ in range(num_layers - 1)
+            )
+        )
 
-        # List to store outputs
-        output = []
-
-        for layer in range(self.num_layers):
-            layer_outputs = []
-            for direction in range(self.directions):
-                h_direction = h[direction + layer * self.directions]
-
-                for t in range(seq_len):
-                    h_direction = self.gru_cells[direction + layer * self.directions](x[t], h_direction)
-                    layer_outputs.append(h_direction)
-
-            # Update the hidden state for the next layer
-            h = torch.stack(layer_outputs).view(self.directions * self.num_layers, seq_len, batch_size, self.hidden_size)
-
-        # Return outputs and the final hidden state
-        output = torch.stack(layer_outputs).view(seq_len, batch_size, self.directions, self.hidden_size)
-        return output, h
+    def forward(self, input, output_lengths, h=None):
+        x = input
+        for i, rnn in enumerate(self.rnn):
+            x, h = rnn(x, output_lengths, h=h)
+        return x, h
 
 
 class DeepSpeech2pacModel(BaseModel):
@@ -88,9 +68,8 @@ class DeepSpeech2pacModel(BaseModel):
 
         rnns = {
             'lstm': nn.LSTM,
-            'gru': nn.GRU,
             'rnn': nn.RNN,
-            'bn_gru': CustomGRULayer
+            'gru': nn.GRU,
         }
 
         self.rnn_bidirectional = rnn_bidirectional
@@ -121,16 +100,14 @@ class DeepSpeech2pacModel(BaseModel):
 
         self.conv_layers = nn.Sequential(*self.conv_layers)
 
-        rnn = rnns[rnn_type]
-        self.rnn = rnn(self.out_conv_dim * self.channels[-1], self.hidden, num_layers=rnn_layers, bidirectional=rnn_bidirectional, batch_first=True)
-        if rnn_bidirectional:
-            self.fc = nn.Linear(in_features=2*hidden, out_features=n_class)
-        else:
-            self.fc = nn.Linear(in_features=hidden, out_features=n_class)
+        self.rnn = BatchRNNLayer(self.out_conv_dim * self.channels[-1], self.hidden, num_layers=rnn_layers, bidirectional=rnn_bidirectional, batch_first=True, rnn_type=nn.GRU)
+        self.fc = nn.Linear(in_features=hidden, out_features=n_class)
 
 
 
     def forward(self, spectrogram, **batch):
+        lengths = batch['spectrogram_length']
+        output_lengths = self.transform_input_lengths(lengths)
         assert len(spectrogram.size()) == 3
         batch_size, dim, time = spectrogram.size()
         assert batch_size == len(batch['audio_path'])
@@ -146,13 +123,10 @@ class DeepSpeech2pacModel(BaseModel):
         output = output.permute(0, 3, 1, 2)
         output = output.view(batch_size, seq_l, channels * dim)
 
-        output, _ = self.rnn(output)
+        output, _ = self.rnn(output, output_lengths)
         batch_size, seq_l, dim = output.size()
         assert batch_size == len(batch['audio_path'])
-        if self.rnn_bidirectional:
-            assert dim == 2*self.hidden
-        else:
-            assert dim == self.hidden
+        assert dim == self.hidden
         output = self.fc(output)
         batch_size, seq_l, dim = output.size()
         assert batch_size == len(batch['audio_path'])
